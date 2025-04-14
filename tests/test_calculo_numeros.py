a6952de1ab413a8488c72c2843742dc12710611b
RenataLanzarini <m.lanzarini@alumno.um.edu.ar>
2025-04-14 09:10:11 -0300

    creación para tests para ingreso de números válidos

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -7,29 +7,16 @@ from unittest.mock import patch
 
 class TestCalculoNumeros(unittest.TestCase):
 
-    @patch(  # este patch controla lo que hace el input
-        'builtins.input',
-        return_value='100'
-    )
-    def test_ingreso_feliz(self, patch_input):
+    @patch('builtins.input', return_value='100')
+    def test_ingreso_numero_positivo(self, mock_input):
         numero = ingrese_numero()
         self.assertEqual(numero, 100)
 
-    @patch(  # este patch controla lo que hace el input
-        'builtins.input',
-        return_value='-100'
-    )
-    def test_ingreso_negativo(self, patch_input):
-        with self.assertRaises(NumeroDebeSerPositivo):
-            ingrese_numero()
-
-    @patch(  # este patch controla lo que hace el input
-        'builtins.input',
-        return_value='AAA'
-    )
-    def test_ingreso_letras(self, patch_input):
-        with self.assertRaises(ValueError):
-            ingrese_numero()
+    @patch('builtins.input', return_value='1')
+    def test_ingreso_numero_positivo_minimo(self, mock_input):
+        numero = ingrese_numero()
+        self.assertEqual(numero, 1)
+   
 
 if __name__ == '__main__':
     unittest.main() 
