e69695966013a2f1d2d8a6ad90534002e57b1100
RenataLanzarini <m.lanzarini@alumno.um.edu.ar>
2025-04-14 13:17:07 -0300

    creación de tests para ingreso de texto no numérico

--- tests/test_calculo_numeros.py
+++ tests/test_calculo_numeros.py
@@ -1,9 +1,8 @@
 import unittest
-from src.exceptions import (
-    ingrese_numero,
-    NumeroDebeSerPositivo,
-)
+
 from unittest.mock import patch
+from src.exceptions import NumeroDebeSerPositivo
+from src.calculo_numeros import ingrese_numero
 
 class TestCalculoNumeros(unittest.TestCase):
 
@@ -17,7 +16,6 @@ class TestCalculoNumeros(unittest.TestCase):
         numero = ingrese_numero()
         self.assertEqual(numero, 1)
 
-
 class TestIngresoNumerosNegativos(unittest.TestCase):
     @patch('builtins.input', return_value='-1')
     def test_numero_negativo_simple(self, mock_input):
@@ -28,7 +26,21 @@ class TestIngresoNumerosNegativos(unittest.TestCase):
     def test_numero_negativo_grande(self, mock_input):
         with self.assertRaises(NumeroDebeSerPositivo):
             ingrese_numero()
+class TestIngresoTextoNoNumerico(unittest.TestCase):
+    @patch('builtins.input', return_value='abc')
+    def test_ingreso_letras(self, mock_input):
+        with self.assertRaises(ValueError):
+            ingrese_numero()
 
+    @patch('builtins.input', return_value='10abc')
+    def test_ingreso_numero_con_letras(self, mock_input):
+        with self.assertRaises(ValueError):
+            ingrese_numero()
+
+    @patch('builtins.input', return_value='@#$')
+    def test_ingreso_simbolos(self, mock_input):
+        with self.assertRaises(ValueError):
+            ingrese_numero()
 
 
 if __name__ == '__main__':
